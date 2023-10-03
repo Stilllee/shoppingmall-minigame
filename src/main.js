115a427ebc742cfd46ef42ecb9bb05b3ac49d3cf
Stilllee <orodae@gmail.com>
2023-10-03 16:21:27 +0900

    ✨ : "UI 및 기능구현 완료"

--- src/main.js
+++ src/main.js
@@ -5,27 +5,70 @@ function loadItems() {
     .then((json) => json.items);
 }
 
-//  Update the list with the given items
-function displayItems(items) {
-  const container = document.querySelector(".items");
-  container.innerHTML = items.map((item) => createHTMLString(item)).join("");
+// Creates HTML element from given item
+function createElement(item) {
+  const img = document.createElement("img");
+  img.setAttribute("class", "thumbnail");
+  img.setAttribute("src", item.image);
+
+  const span = document.createElement("span");
+  span.setAttribute("class", "description");
+  span.innerText = `${item.gender}, ${item.size} size`;
+
+  const li = document.createElement("li");
+  li.setAttribute("class", "item");
+  li.setAttribute("data-type", item.type);
+  li.setAttribute("data-color", item.color);
+  li.append(img);
+  li.append(span);
+
+  return li;
 }
 
-// Create HTML list item from the given data item
-function createHTMLString(item) {
-  return `
-    <li class="item">
-        <img src="${item.image}" alt="${item.type}" class="item__thumbnail" />
-        <span class="item__description">${item.gender}, ${item.size}</span>
-    </li>
-  `;
+// Handle button click
+function onButtonClick(event, items) {
+  const dataset = event.target.dataset;
+  const key = dataset.key;
+  const value = dataset.value;
+
+  if (key == null || value == null) {
+    return;
+  }
+
+  updateItems(items, key, value);
 }
 
-// main
+// Make the items matching {key: value} invisible.
+function updateItems(items, key, value) {
+  items.forEach((item) => {
+    if (item.dataset[key] === value) {
+      item.classList.remove("invisible");
+    } else {
+      item.classList.add("invisible");
+    }
+  });
+}
+
+// Update the all items to be visible.
+function showAllItems(items) {
+  items.forEach((item) => item.classList.remove("invisible"));
+}
+
 loadItems()
   .then((items) => {
-    console.log(items);
-    displayItems(items);
-    // setEventListeners(items);
+    const elements = items.map(createElement);
+    const container = document.querySelector(".items");
+    container.append(...elements);
+    const buttons = document.querySelector(".buttons");
+    buttons.addEventListener("click", (event) =>
+      onButtonClick(event, elements)
+    );
+
+    const logo = document.querySelector(".logo");
+    logo.addEventListener("click", () => showAllItems(elements));
+
+    if (container.scrollHeight > container.clientHeight) {
+      container.classList.add("scrollable");
+    }
   })
   .catch(console.log);
